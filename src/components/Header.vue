b1d75553ca134797a411300e86d0e0ba46e80cfe
Charles Ballew <charleswballewjr@gmail.com>
2018-03-13 17:19:40 -0600

    added capital letters to rout names

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="header">
     <nav class="navbar sticky-top navbar-light bg-light">
-      <router-link class="nav-link" :to="{ name: 'home'}">
+      <router-link class="nav-link" :to="{ name: 'Home'}">
         Campaign Chat
       </router-link>
-      <router-link class="nav-link" :to="{ name: 'home'}">
+      <router-link class="nav-link" :to="{ name: 'Home'}">
         Home
       </router-link>
-      <router-link class="nav-link" :to="{ name: 'players'}">
+      <router-link class="nav-link" :to="{ name: 'PlayersList'}">
         Players
       </router-link>
       <router-link class="nav-link" :to="{ name: 'PolarChart'}">
